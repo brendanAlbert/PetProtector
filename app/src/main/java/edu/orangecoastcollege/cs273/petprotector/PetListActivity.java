ec91e5da916645ef14560e2538e496e24b15246c
brendanAlbert <alber123@mail.chapman.edu>
2017-10-26 16:50:06 -0700

    Updated Javadocs

--- app/src/main/java/edu/orangecoastcollege/cs273/petprotector/PetListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/petprotector/PetListActivity.java
@@ -18,14 +18,41 @@ import android.widget.ImageView;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * PetListActivity is the main controller for the PetProtector app.
+ *
+ * This app was instructional in how to handle requesting User permissions and using a Universal
+ * Resource Identifier (URI).
+ *
+ * The User's permission is required to be able to access their camera or external storage, such
+ * as the images in their Image Gallery.
+ *
+ * Constants are created for permission granted or denied.
+ *
+ * The onCreate method sets the content view and wires up the pet Image View.
+ * The pet Image View gets the image from a URI.
+ *
+ * The selectPetImage method is called when the User taps the ImageView.
+ * If this is the first time, then the permissions will be requested.
+ * Once the permissions are allowed, the onActivityResult method is called.
+ *
+ * onActivityResult is called and will display the Image Gallery if the user
+ * allowed access.
+ */
 public class PetListActivity extends AppCompatActivity {
 
     private ImageView petImageView;
+    private Uri imageUri;
 
     // Constants for permissions:
     private static final int GRANTED = PackageManager.PERMISSION_GRANTED;
     public static final int DENIED = PackageManager.PERMISSION_DENIED;
 
+    /**
+     * The onCreate method sets the content view and wires up the pet Image View.
+     * The pet Image View gets the image from a URI we created.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +62,13 @@ public class PetListActivity extends AppCompatActivity {
         petImageView.setImageURI(getUriFromResource(this, R.drawable.none));
     }
 
+    /**
+     * getUriFromResource is called from onCreate to set the initial ImageView.
+     *
+     * @param context
+     * @param resId
+     * @return
+     */
     public static Uri getUriFromResource(Context context, int resId)
     {
         Resources res = context.getResources();
@@ -49,6 +83,15 @@ public class PetListActivity extends AppCompatActivity {
         return Uri.parse(uri);
     }
 
+    /**
+     * The selectPetImage method is called when the User taps the ImageView.
+     * If this is the first time, then the permissions will be requested.
+     * This is performed by building a list of denied permissions.
+     * Once the permissions are allowed, the onActivityResult method is called.
+     * The URI is created dynamically
+     * so this static method is incredibly versatile
+     * @param v
+     */
     public void selectPetImage(View v)
     {
         List<String> permissionsList = new ArrayList<>();
@@ -84,6 +127,25 @@ public class PetListActivity extends AppCompatActivity {
             Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
             // Start activity for a result (picture)
             startActivityForResult(galleryIntent, 1);
+        }
+    }
+
+    /**
+     * onActivityResult is called and will display the Image Gallery if the user
+     * allowed access.
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null)
+        {
+            // data is the data from GalleryIntent (the URI of some image)
+            imageUri = data.getData();
+            petImageView.setImageURI(imageUri);
         }
     }
 }
